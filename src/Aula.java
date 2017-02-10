8b1e9ede198baab97685d9049de2ed3d0ea6074d
Johnathan <johnathan.stark42@gmail.com>
2017-02-10 09:10:33 -0200

    cod

--- src/Aula.java
+++ src/Aula.java
@@ -1,9 +1,22 @@
+import javax.swing.*;
+
 /**
  * Created by Léllo Arruda on 09/02/2017.
  */
+
+/* Aurelio um peograma que pega dois valores e multiplica
+*  cria variaveis do int , Usa a classe JOptionPane.showInputDialog()
+*  para buscar o valor e atribuir nas variaveis
+*  multiplica os valores e exibir na tela com JOptionPane.showConfirmDialog() */
+
+
+
+
 public class Aula {
 
     public static void main(String[] args){
+
+
 
     }
 }
