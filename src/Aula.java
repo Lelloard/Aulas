6b8fcdae13e93a9c39d415934de4b2c26ca2ee59
Léllo Arruda <aurelioarruda@live.com>
2017-02-11 06:57:39 -0300

    olha essa merda logo,kkk

--- src/Aula.java
+++ src/Aula.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by Léllo Arruda on 09/02/2017.
@@ -17,6 +18,12 @@ public class Aula {
 
     public static void main(String[] args){
 
+        int numero1, numero2, valor3;
+        numero1 = Integer.parseInt(JOptionPane.showInputDialog("coloque o primeiro numero"));
+        numero2 = Integer.parseInt(JOptionPane.showInputDialog("coloque o segundo numero"));
+        valor3 = numero1 / numero2;
+
+        JOptionPane.showMessageDialog(  null,String.valueOf(valor3), "resultado",JOptionPane.INFORMATION_MESSAGE);
 
 
     }
