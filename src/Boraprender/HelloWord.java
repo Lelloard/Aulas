623fb1e67c8bb88efc69c438b9103c5a626a43fd
Léllo Arruda <aurelioarruda@live.com>
2017-02-16 01:37:34 -0300

    olha essa merda logo,kkk

---
+++ src/Boraprender/HelloWord.java
@@ -0,0 +1,10 @@
+package Boraprender;
+
+/**
+ * Created by Léllo Arruda on 16/02/2017.
+ */
+public class HelloWord {
+            public static void main ( String[] args){
+                System.out.print("Oi Mundo");
+            }
+}
--- src/aula1/Calcula.java
+++ src/aula1/Calcula.java
@@ -10,13 +10,14 @@ public class Calcula {
 
     public static void  main (String[] args){
 
-        int numero1 , numero2 , numero3;
+        double numero1 , numero2 , numero3, numero4;
 
-       numero1 = Integer.parseInt( JOptionPane.showInputDialog("Coloque o primeiro numero"));
+       numero1 =    Integer.parseInt( JOptionPane.showInputDialog("Coloque o primeiro numero"));
         numero2 = Integer.parseInt( JOptionPane.showInputDialog("Coloque o segundo numero"));
-        numero3 = numero1 + numero2;
+        numero3 = Integer.parseInt(JOptionPane.showInputDialog("coloque o terceiro numero"));
+        numero4 = numero1 + numero2 + numero3;
 
-        JOptionPane.showMessageDialog(null,String.valueOf(numero3),"Resultado",JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null,String.valueOf(numero4),"Resultado",JOptionPane.INFORMATION_MESSAGE);
 
     }
 }
